b90c5bfae19e206eb382d549389eb972d1112832
DESKTOP-P7SURMK\Lien <screen.leon@gmail.com>
2017-11-01 04:40:58 +0800

    now can set all card into the deck

--- homework/hw2/JavaApplication8/src/ntou/cs/java2017/ChenLiEn/hw2/DeckOfUNOCards.java
+++ homework/hw2/JavaApplication8/src/ntou/cs/java2017/ChenLiEn/hw2/DeckOfUNOCards.java
@@ -4,11 +4,48 @@
  * and open the template in the editor.
  */
 package ntou.cs.java2017.ChenLiEn.hw2;
+import java.security.SecureRandom;
+import ntou.cs.java2017.ChenLiEn.hw2.UNOCard.UNOType;
+import ntou.cs.java2017.ChenLiEn.hw2.UNOCard.ColorType;
 
 /**
  *
  * @author user
  */
 public class DeckOfUNOCards {
+    private UNOCard[] deck;
+    private int currentCard;
+    private static final int COLOR_NUMBER_OF_CARD = 12;
+    private static final int NUMBER_OF_CARD = 48;
+    private static final SecureRandom randomNumbers = new SecureRandom();
     
+    public DeckOfUNOCards(){
+        ColorType colorType = null;
+        deck = new UNOCard[NUMBER_OF_CARD];
+        this.currentCard = 0;
+        for(int _i = 0; _i < deck.length; _i++){
+            int _j = 0;
+            _j = _i % COLOR_NUMBER_OF_CARD;
+            switch(_j){
+                case 0:
+                    colorType = ColorType.BLUE;
+                    break;
+                case 1:
+                    colorType = ColorType.GREEN;
+                    break;
+                case 2:
+                    colorType = ColorType.RED;
+                    break;
+                case 3:
+                    colorType = ColorType.YELLOW;
+                    break;
+            }
+            if(_i % COLOR_NUMBER_OF_CARD == 10)
+                deck[_i] = new UNOCard(UNOType.SKIP, colorType, -1);
+            else if(_i % COLOR_NUMBER_OF_CARD == 11){
+                deck[_i] = new UNOCard(UNOType.DRAW2, colorType, -1);
+            }else
+                deck[_i] = new UNOCard(UNOType.NUMBER, colorType, _i % COLOR_NUMBER_OF_CARD);
+        }
+    }
 }
