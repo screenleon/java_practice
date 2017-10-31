c3c9baa105771527eb22df854c3ca51a565c12c4
DESKTOP-P7SURMK\Lien <screen.leon@gmail.com>
2017-10-31 21:46:06 +0800

    hw2_1 finished

--- homework/hw2/JavaApplication8/src/ntou/cs/java2017/ChenLiEn/hw2/CrapsSimulation.java
+++ homework/hw2/JavaApplication8/src/ntou/cs/java2017/ChenLiEn/hw2/CrapsSimulation.java
@@ -4,11 +4,91 @@
  * and open the template in the editor.
  */
 package ntou.cs.java2017.ChenLiEn.hw2;
+import java.security.SecureRandom;
 
 /**
  *
  * @author user
  */
 public class CrapsSimulation {
+    private static final SecureRandom randomNumbers = new SecureRandom();
+    private enum Status{ CONTINUE, WON, LOST, UNSTART };
     
+    private static final int SNAKE_EYES = 2;
+    private static final int TREY = 3;
+    private static final int SEVEN = 7;
+    private static final int YO_LEVEN = 11;
+    private static final int BOX_CARS = 12;
+    
+    private int die1, die2, firstDieSum, sum, rollTime;
+    private Status gameStatus;
+    
+    public CrapsSimulation(){
+        this.gameStatus = Status.UNSTART;
+        this.die1 = 0;
+        this.die2 = 0;
+        this.sum = 0;
+        this.rollTime = 0;
+        this.firstDieSum = 0;
+    }
+    
+    public void startRoll(){
+        this.rollDice();
+    }
+    
+    public void rollDice(){
+        this.rollTime++;
+        this.die1 = 1 + randomNumbers.nextInt(6);
+        this.die2 = 1 + randomNumbers.nextInt(6);
+        this.sum = this.die1 + this.die2;
+        this.judgeCard();
+    }
+    
+    public void judgeCard(){
+        if(this.firstDieSum != 0){
+            if(this.sum == this.firstDieSum)
+                this.gameStatus = Status.WON;
+            else
+                if(this.sum == SEVEN)
+                    this.gameStatus = Status.LOST;
+        }else{
+            this.firstDieSum = this.sum;
+            switch(this.sum){
+            case SEVEN:
+            case YO_LEVEN:
+                this.gameStatus = Status.WON;
+                break;
+            case SNAKE_EYES:
+            case TREY:
+            case BOX_CARS:
+                this.gameStatus = Status.LOST;
+                break;
+            default:
+                this.gameStatus = Status.CONTINUE;
+                break;
+            }
+        }
+        if(this.gameStatus == Status.CONTINUE)
+            this.rollDice();
+        else return;
+    }
+    
+    public void initialCrap(){
+        this.gameStatus = Status.UNSTART;
+        this.die1 = 0;
+        this.die2 = 0;
+        this.sum = 0;
+        this.rollTime = 0;
+        this.firstDieSum = 0;
+    }
+    
+    public boolean getWinLost(){
+        if(this.gameStatus == Status.WON)
+            return true;
+        else return false;
+    }
+    
+    public int getRollTime(){
+        return this.rollTime;
+    }
 }
--- homework/hw2/JavaApplication8/src/ntou/cs/java2017/ChenLiEn/hw2/CrapsSimulationTest.java
+++ homework/hw2/JavaApplication8/src/ntou/cs/java2017/ChenLiEn/hw2/CrapsSimulationTest.java
@@ -5,10 +5,49 @@
  */
 package ntou.cs.java2017.ChenLiEn.hw2;
 
+import java.security.SecureRandom;
+
 /**
  *
  * @author user
  */
 public class CrapsSimulationTest {
     
+    public static void main(String[] args){
+        int roundMax = 1000, winRound = 0, lostRound = 0;
+        double winRate = 0.0, roundLength = 0.0;
+        int[][] round = new int[22][2];
+        CrapsSimulation craps = new CrapsSimulation();
+        
+        for(int _i = 0, rollTime = 0; _i < roundMax; _i++){
+            craps.startRoll();
+            rollTime = craps.getRollTime();
+            if(craps.getWinLost()){
+                if(rollTime >= 21)
+                    round[21][0]++;
+                else round[rollTime][0]++;
+                winRound++;
+            }else{
+                if(rollTime >= 21)
+                    round[21][1]++;
+                else round[rollTime][1]++;
+                lostRound++;
+            }
+            craps.initialCrap();
+        }
+        for(int _i = 1; _i < 22; _i++){
+            if(_i > 20){
+                System.out.printf("%d games won and %d games lost on rolls after the 20th roll\n", round[_i][0], round[_i][1]);
+            }else{
+                System.out.printf("%d games won and %d games lost on rolls #%d\n", round[_i][0], round[_i][1], _i);
+            }
+        }
+        
+        winRate = winRound * 1.0 / 10;
+        for(int _i = 1; _i < 22; _i++)
+            roundLength = roundLength + (round[_i][0] + round[_i][1]) * _i;
+        System.out.printf("The chances of winning are %d / 1000 = %2.2f%%\n", winRound, winRate);
+        System.out.printf("The average game length is %.2f rolls.\n", roundLength /1000);
+    }
+
 }
