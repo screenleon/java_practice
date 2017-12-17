d818eef23200ca38330f629311bbb403d99be414
LEON\user <screen.leon@gmail.com>
2017-12-17 11:43:19 +0800

    create hw4-1, hw4-2 classes

---
+++ homework/hw4/JavaApplication9/src/ntou/cs/java2017/ChenLiEn/hw4/WeatherViewer.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ntou.cs.java2017.ChenLiEn.hw4;
+
+/**
+ *
+ * @author user
+ */
+public class WeatherViewer {
+    
+}
