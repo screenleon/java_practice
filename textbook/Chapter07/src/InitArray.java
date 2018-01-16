e25c1e266475e4ca02e93a3fd2eb24504372f410
LEON\user <screen.leon@gmail.com>
2018-01-16 22:56:24 +0800

    7-1-3 textbook code

--- textbook/Chapter07/src/InitArray.java
+++ textbook/Chapter07/src/InitArray.java
@@ -10,7 +10,12 @@
  */
 public class InitArray {
     public static void main(String[] args){
-        int[] array = { 32, 27, 64, 18, 95, 14, 90, 70, 60, 37 };
+        final int ARRAY_LENGTH = 10;
+        int[] array = new int[ARRAY_LENGTH];
+        
+        for(int counter = 0; counter < array.length; counter++){
+            array[counter] = 2 + 2 * counter;
+        }
         //%n will auto change to nextline in window will be \r\n 
         //in linux will be \n
         System.out.printf("%s%8s%n", "Index", "Value");
