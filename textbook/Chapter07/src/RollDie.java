e3da41ebcd598ce89a149dfa3ce8bc5bb8d99bf5
LEON\user <screen.leon@gmail.com>
2018-01-16 23:51:54 +0800

    7-4 textbook code

---
+++ textbook/Chapter07/src/RollDie.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+import java.security.SecureRandom;
+
+/**
+ *
+ * @author user
+ */
+public class RollDie {
+    public static void main(String[] args){
+        SecureRandom randomNumbers = new SecureRandom();
+        int[] frequency = new int[7];
+        
+        for(int roll = 1; roll < 6000000; roll++){
+            ++frequency[1 + randomNumbers.nextInt(6)];
+        }
+        
+        System.out.printf("%s%10s%n", "Face", "Frequency");
+        
+        for(int face = 1; face < frequency.length; face++)
+            System.out.printf("%4d%10d%n", face, frequency[face]);
+    }
+}
