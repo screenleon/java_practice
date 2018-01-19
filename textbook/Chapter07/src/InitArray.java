d186ce5ee12899256e09c531ca17536b26f8bb17
LEON\user <screen.leon@gmail.com>
2018-01-20 03:20:22 +0800

    7-1-5 textbook code

--- textbook/Chapter07/src/InitArray.java
+++ textbook/Chapter07/src/InitArray.java
@@ -10,24 +10,25 @@
  */
 public class InitArray {
     public static void main(String[] args){
-        int[][] array1 = {{1, 2, 3}, {4, 5, 6}};
-        int[][] array2 = {{1, 2}, {3}, {4, 5, 6}};
-        
-        //%n will auto change to nextline in window will be \r\n 
-        //in linux will be \n
-        System.out.println("Values in array1 by row are");
-        outputArray(array1);
-        
-        System.out.printf("%nValues in array2 by row are%n");
-        outputArray(array2);
-    }
-    
-    public static void outputArray(int[][] array){
-        for(int row = 0; row < array.length; row++){
-            for(int column = 0; column < array[row].length; column++){
-                System.out.printf("%d  ", array[row][column]);
+        if(args.length != 3)
+            System.out.printf("Error: Please re-enter the entire command, including%n" +
+                    "an array size, initial value and increment.%n");
+        else{
+            int arrayLength = Integer.parseInt(args[0]);
+            int[] array = new int[arrayLength];
+            
+            int initialValue = Integer.parseInt(args[1]);
+            int increment = Integer.parseInt(args[2]);
+            
+            for(int counter = 0; counter < array.length; counter++){
+                array[counter] = initialValue + increment * counter;
             }
-            System.out.println();
+            
+            System.out.printf("%s%8s%n", "Index", "Value");
+            
+            for(int counter = 0; counter < array.length; counter++)
+                System.out.printf("%5d%8d%n", counter, array[counter]);
         }
     }
+    
 }
