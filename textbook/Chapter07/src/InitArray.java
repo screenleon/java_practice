9a8b349f38d7d134bb69e4283135ec84c687a059
LEON\user <screen.leon@gmail.com>
2018-01-20 03:00:28 +0800

    7-1-4 textbook code

--- textbook/Chapter07/src/InitArray.java
+++ textbook/Chapter07/src/InitArray.java
@@ -10,18 +10,24 @@
  */
 public class InitArray {
     public static void main(String[] args){
-        final int ARRAY_LENGTH = 10;
-        int[] array = new int[ARRAY_LENGTH];
+        int[][] array1 = {{1, 2, 3}, {4, 5, 6}};
+        int[][] array2 = {{1, 2}, {3}, {4, 5, 6}};
         
-        for(int counter = 0; counter < array.length; counter++){
-            array[counter] = 2 + 2 * counter;
-        }
         //%n will auto change to nextline in window will be \r\n 
         //in linux will be \n
-        System.out.printf("%s%8s%n", "Index", "Value");
+        System.out.println("Values in array1 by row are");
+        outputArray(array1);
         
-        for(int counter = 0; counter < array.length; counter++){
-            System.out.printf("%5d%8d%n", counter, array[counter]);
+        System.out.printf("%nValues in array2 by row are%n");
+        outputArray(array2);
+    }
+    
+    public static void outputArray(int[][] array){
+        for(int row = 0; row < array.length; row++){
+            for(int column = 0; column < array[row].length; column++){
+                System.out.printf("%d  ", array[row][column]);
+            }
+            System.out.println();
         }
     }
 }
