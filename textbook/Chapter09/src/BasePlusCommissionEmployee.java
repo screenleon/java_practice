747f675319e839750bc0ad780165a1f034d12d92
LEON\user <screen.leon@gmail.com>
2018-01-29 01:16:11 +0800

    9-2-2 textbook code

--- textbook/Chapter09/src/BasePlusCommissionEmployee.java
+++ textbook/Chapter09/src/BasePlusCommissionEmployee.java
@@ -8,66 +8,18 @@
  *
  * @author user
  */
-public class BasePlusCommissionEmployee {
-    private final String firstName;
-    private final String lastName;
-    private final String socialSecurityNumber;
-    private double grossSales;
-    private double commissionRate;
+public class BasePlusCommissionEmployee extends CommissionEmployee{
     private double baseSalary;
     
     public BasePlusCommissionEmployee(String _firstName, String _lastName,
             String _socialSecurityNumber, double _grossSales, double _commissionRate,
             double _baseSalary){
-        if(_grossSales < 0.0)
-            throw new IllegalArgumentException("Gross sales must be >= 0.0");
-        
-        if(_commissionRate <= 0.0 || _commissionRate >= 1.0)
-            throw new IllegalArgumentException("Commission rate must be > 0.0 and < 1.0");
+        super(_firstName, _lastName, _socialSecurityNumber, _grossSales, _commissionRate);
         
         if(_baseSalary < 0.0)
             throw new IllegalArgumentException("Base salary must be >= 0.0");
         
-        this.firstName = _firstName;
-        this.lastName = _lastName;
-        this.socialSecurityNumber = _socialSecurityNumber;
-        this.grossSales = _grossSales;
-        this.commissionRate = _commissionRate;
         this.baseSalary = _baseSalary;
-    }
-    
-    public String getFirstName(){
-        return this.firstName;
-    }
-    
-    public String getLastName(){
-        return this.lastName;
-    }
-    
-    public String getSocialSecurityNumber(){
-        return this.socialSecurityNumber;
-    }
-    
-    public void setGrossSales(double _grossSales){
-        if(_grossSales < 0.0)
-            throw new IllegalArgumentException("Gross sales must be >= 0.0");
-        
-        this.grossSales = _grossSales;
-    }
-    
-    public double getGrossSales(){
-        return this.grossSales;
-    }
-    
-    public void setCommissionRate(double _commissionRate){
-        if(_commissionRate <= 0.0 || _commissionRate >= 1.0)
-            throw new IllegalArgumentException("Commission rate must be > 0.0 and < 1.0");
-        
-        this.commissionRate = _commissionRate;
-    }
-    
-    public double getCommissionRate(){
-        return this.commissionRate;
     }
     
     public void setBaseSalary(double _baseSalary){
@@ -81,16 +33,17 @@ public class BasePlusCommissionEmployee {
         return this.baseSalary;
     }
     
+    @Override
     public double earnings(){
-        return this.baseSalary + (this.commissionRate * this.grossSales);
+        return this.baseSalary + (commissionRate * grossSales);
     }
     
     @Override
     public String toString(){
         return String.format("%s: %s %s%n%s: %s%n%s: %.2f%n%s: %.2f%n%s: %.2f",
-                "basse-salaried commission employee", this.firstName, this.lastName,
-                "social security number", this.socialSecurityNumber, "gross sales",
-                this.grossSales, "commission rate", this.commissionRate,
+                "basse-salaried commission employee", firstName, lastName,
+                "social security number", socialSecurityNumber, "gross sales",
+                grossSales, "commission rate", commissionRate,
                 "base salary", this.baseSalary);
     }
 }
