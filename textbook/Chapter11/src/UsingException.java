f22cafd5beb2418e11fb19b17b3447e6199f9e78
LEON\user <screen.leon@gmail.com>
2018-01-31 14:55:01 +0800

    11-2-2 textbook code

--- textbook/Chapter11/src/UsingException.java
+++ textbook/Chapter11/src/UsingException.java
@@ -11,35 +11,35 @@
 public class UsingException {
     public static void main(String[] args){
         try{
-            throwException();
+            method1();
         }catch(Exception exception){
-            System.err.println("Exception handled in main");
+            System.err.printf("%s%n%n", exception.getMessage());
+            exception.printStackTrace();
+            System.out.println("Error print");
+            StackTraceElement[] traceElements = exception.getStackTrace();
+            
+            System.out.printf("%nStack trace from getStackTrace:%n");
+            System.out.println("Class\t\tFile\t\t\tLine\tMethod");
+            
+            for(StackTraceElement element : traceElements){
+                System.out.printf("%s\t", element.getClassName());
+                System.out.printf("%s\t", element.getFileName());
+                System.out.printf("%s\t", element.getLineNumber());
+                System.out.printf("%s%n", element.getMethodName());
+            }
         }
         
-        doesNotThrowException();
     }
     
-    public static void throwException() throws Exception{
-        try{
-            System.out.println("Method throw Exception");
-            throw new Exception();
-        }catch(Exception exception){
-            System.err.println("Exception handled in method throw Exception");
-            throw exception;
-        }finally{
-            System.err.println("Finally executed in throwException");
-        }
+    public static void method1() throws Exception{
+        method2();
     }
     
-    public static void doesNotThrowException(){
-        try{
-            System.out.println("Method doesNotThrowException");
-        }catch(Exception exception){
-            System.err.println(exception);
-        }finally{
-            System.err.println("Finally executed in doesNotThrowException");
-        }
-        
-        System.out.println("End of method doesNotThrowException");
+    public static void method2() throws Exception{
+        method3();
+    }
+    
+    public static void method3() throws Exception{
+        throw new Exception("Exception thrown in method3");
     }
 }
