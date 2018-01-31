8330cad1df362decae8b48b0b050d66e6984b6d4
LEON\user <screen.leon@gmail.com>
2018-01-31 14:27:40 +0800

    11-1-2 textbook code

--- textbook/Chapter11/src/DivideByZeroNoExecptionHandling.java
+++ textbook/Chapter11/src/DivideByZeroNoExecptionHandling.java
@@ -4,25 +4,42 @@
  * and open the template in the editor.
  */
 import java.util.Scanner;
+import java.util.InputMismatchException;
 
 /**
  *
  * @author user
  */
 public class DivideByZeroNoExecptionHandling {
-    public static int quotient(int numerator, int denominator){
+    public static int quotient(int numerator, int denominator) throws ArithmeticException{
         return numerator / denominator;
     }
     
     public static void main(String[] args){
         Scanner scanner = new Scanner(System.in);
+        boolean continueLoop = true;
+        do{
+            try{
+                System.out.print("Please enter an integer numerator: ");
+                int numerator = scanner.nextInt();
+                System.out.print("Please enter an integer denominator: ");
+                int denominator = scanner.nextInt();
+                
+                int result = quotient(numerator, denominator);
+                System.out.printf("%nResult: %d / %d = %d%n", numerator, denominator, result);
+                continueLoop = false;
+            }catch(InputMismatchException inputMismatchExecption){
+                System.err.printf("%nExecption: %s%n", inputMismatchExecption);
+                scanner.nextLine();       //discard input so user can try again
+                System.out.printf("You must enter integers. Please try again.%n%n");
+            }catch(ArithmeticException arithmeticException){
+                System.err.printf("%nExecption: %s%n", arithmeticException);
+                System.out.printf("Zero is an invalid denominator. Please try again.%n%n");
+            }
+        }while(continueLoop);
         
-        System.out.print("Please enter an integer numerator: ");
-        int numerator = scanner.nextInt();
-        System.out.print("Please enter an integer denominator: ");
-        int denominator = scanner.nextInt();
         
-        int result = quotient(numerator, denominator);
-        System.out.printf("%nResult: %d / %d = %d%n", numerator, denominator, result);
+        
+        
     }
 }
