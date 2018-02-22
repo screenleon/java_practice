f99f765c42e09c158e978220d2a1f7e0a3c45204
LEON\user <screen.leon@gmail.com>
2018-02-22 11:58:41 +0800

    12-3 textbook code

---
+++ textbook/Chapter12/src/TextFieldTest.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author user
+ */
+public class TextFieldTest {
+    public static void main(String[] args){
+        TextFieldFrame textFieldFrame = new TextFieldFrame();
+        textFieldFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        textFieldFrame.setSize(350, 100);
+        textFieldFrame.setVisible(true);
+    }
+}
